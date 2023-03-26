eecf7f687cece180cc0138f86dde853aa71a2ef6
TahiKari <tahkar@outloo.fr>
2023-03-26 22:37:32 +0200

    Maj cours js

--- 01_Studi/04_Front_End/03_Learn_Program_Js/02-Test.js
+++ 01_Studi/04_Front_End/03_Learn_Program_Js/02-Test.js
@@ -1,12 +1,17 @@
-let numbers = [3, 8, 5, 9, 1];
-let numbersLength = numbers.length;
-for (let main = 0; main < numbersLength - 1; main++) {
-	for (let cell = 0; cell < numbersLength - main - 1; cell++) {
-	  if (numbers[cell] > numbers[cell + 1]) { // Si la valeur de la case actuelle est supérieure à la valeur de la case d'après, on permute les valeurs (en utilisant l'échange de variables)
-     numbers[cell] = numbers[cell] + numbers[cell + 1];
-		 numbers[cell + 1] = numbers[cell] - numbers[cell + 1];
-		 numbers[cell] = numbers[cell] - numbers[cell + 1];
+function bubbleSort(items) {
+    var length = items.length;  
+    for (var i = 0; i < length; i++) {
+          for (var j = 0; j < length - i - 1; j++) {
+                   if(items[j] > items[j+1]) {
+                           var tmp = items[j]; 
+                items[j] = items[j+1]; 
+                items[j+1] = tmp; 
+            }
+        }        
     }
-  }
 }
-console.log(numbers);
+
+var arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]; 
+bubbleSort(arr);
+
+console.log(arr);
--- 01_Studi/04_Front_End/03_Learn_Program_Js/10_The_Tables/04_Bubble_Sorting/01_Ex.js
+++ 01_Studi/04_Front_End/03_Learn_Program_Js/10_The_Tables/04_Bubble_Sorting/01_Ex.js
@@ -1,12 +1,15 @@
-let numbers = [3, 8, 5, 9, 1];
-let numbersLength = numbers.length;
-for (let main = 0; main < numbersLength - 1; main++) {
-	for (let cell = 0; cell < numbersLength - main - 1; cell++) {
-	  if (numbers[cell] > numbers[cell + 1]) { // Si la valeur de la case actuelle est supérieure à la valeur de la case d'après, on permute les valeurs (en utilisant l'échange de variables)
-     numbers[cell] = numbers[cell] + numbers[cell + 1];
-		 numbers[cell + 1] = numbers[cell] - numbers[cell + 1];
-		 numbers[cell] = numbers[cell] - numbers[cell + 1];
+let persons = ['jules', 'laure', 'vincent', 'emma'];
+let personsLength = persons.length;
+		
+for (let main = 0; main < personsLength - 1; main++) {
+  for (let cell = 0; cell < personsLength - main - 1; cell++) {
+    if (persons[cell] > persons[cell + 1]) {
+
+     let temp;
+     temp = persons[cell];
+     persons[cell] = persons[cell + 1];
+     persons[cell + 1] = temp;
     }
   }
 }
-console.log(numbers);
+console.log(persons);
