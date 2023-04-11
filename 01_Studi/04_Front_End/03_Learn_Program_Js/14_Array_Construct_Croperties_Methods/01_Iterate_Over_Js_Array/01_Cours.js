63269c5666f203abaca1dd4331a4299757136397
TahiKari <tahkar@outloo.fr>
2023-04-11 10:28:40 +0200

    Maj Cours Js

--- 01_Studi/04_Front_End/03_Learn_Program_Js/14_Array_Construct_Croperties_Methods/01_Iterate_Over_Js_Array/01_Cours.js
+++ 01_Studi/04_Front_End/03_Learn_Program_Js/14_Array_Construct_Croperties_Methods/01_Iterate_Over_Js_Array/01_Cours.js
@@ -7,6 +7,15 @@
   // Display 51 in the console because it's the first value found to be greater than 10
   console.log(numbers.find(isGreaterThanTen))*/
 
+  /*function isGreaterThanTen(element) {
+    return element > 10
+  }
+  
+  let numbers = [0, 9, 51, 100, 6, 18, 2]
+  
+  // Display 2 in the console because it is the index of 51 which is the first value found to be greater than 10.
+  console.log(numbers.findIndex(isGreaterThanTen))*/
+
   /*let animals = ['Lion', 'Chat', 'Chien', 'Lion', 'Ours']
 
   // Display 0 in the console because the first 'Lion' value found is at index 0, even if there is a second one at index 3.
@@ -14,50 +23,51 @@
   
   // Display 3 in the console because the first 'Lion' value found when starting to browse from the index 2 is at index 3.
   console.log(animals.indexOf('Lion', 2))*/
-  
-  /*let animals = ['Lion', 'Chat', 'Chien', 'Lion', 'Ours']
 
-  // Display 3 in the console because the last 'Lion' value found is at index 3.
-  console.log(animals.lastIndexOf('Lion'))
-  
-  // Display 0 in the console because the last 'Lion' value found when starting to browse from the index 1 is at index 0.
-  // As the search is from right to left and starts at index 1 ('Chat') then the last 'Lion' value found is at index 0.
-  console.log(animals.lastIndexOf('Lion', 1))*/
-  
-  /*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
+/*let animals = ['Lion', 'Chat', 'Chien', 'Lion', 'Ours']
 
-  let numbers = [0, 900, 1000, 11, 40, 6, 8, 2, 61]
-  
-  // Display in the console : ['Chat', 'Cheval', 'Chien', 'Lion', 'Ours', 'Tigre']
-  console.log(animals.sort())
-  
-  // Display in the console : [0, 1000, 11, 2, 40, 6, 61, 8, 900]
-  console.log(numbers.sort())
-  
-  // Function for comparing the length of strings in order to sort them in ascending order
-  function compareTextLength(a, b) {
-    if (a.length > b.length) {
-      return 1
-    } else if (a.length < b.length) {
-      return -1
-    }
-  
-    // The case where a.length = b.length
-    return 0
+// Display 3 in the console because the last 'Lion' value found is at index 3.
+console.log(animals.lastIndexOf('Lion'))
+
+// Display 0 in the console because the last 'Lion' value found when starting to browse from the index 1 is at index 0.
+// As the search is from right to left and starts at index 1 ('Chat') then the last 'Lion' value found is at index 0.
+console.log(animals.lastIndexOf('Lion', 1))*/
+
+/*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
+
+let numbers = [0, 900, 1000, 11, 40, 6, 8, 2, 61]
+
+// Display in the console : ['Chat', 'Cheval', 'Chien', 'Lion', 'Ours', 'Tigre']
+console.log(animals.sort())
+
+// Display in the console : [0, 1000, 11, 2, 40, 6, 61, 8, 900]
+console.log(numbers.sort())
+
+// Function for comparing the length of strings in order to sort them in ascending order
+function compareTextLength(a, b) {
+  if (a.length > b.length) {
+    return 1
+  } else if (a.length < b.length) {
+    return -1
   }
-  
-  // Display in the console : ['Chat', 'Lion', 'Ours', 'Chien', 'Tigre', 'Cheval']
-  // 'Chat' has 4 characters, 'Cheval' has 6
-  console.log(animals.sort(compareTextLength))
-  
-  // Function for comparing numbers in order to sort them in ascending order
-  function compareNumbers(a, b) {
-    return a - b
-  }
-  
-  // Display in the console : [0, 2, 6, 8, 11, 40, 61, 900, 1000]
-  console.log(numbers.sort(compareNumbers))*/
-  
+
+  // The case where a.length = b.length
+  return 0
+}
+
+// Display in the console : ['Chat', 'Lion', 'Ours', 'Chien', 'Tigre', 'Cheval']
+// 'Chat' has 4 characters, 'Cheval' has 6
+console.log(animals.sort(compareTextLength))
+
+// Function for comparing numbers in order to sort them in ascending order
+function compareNumbers(a, b) {
+  return a - b
+}
+
+// Display in the console : [0, 2, 6, 8, 11, 40, 61, 900, 1000]
+console.log(numbers.sort(compareNumbers))*/
+
+
 /*function isTextLengthGreaterThan4(element) {
   return element.length > 4
 }
@@ -80,6 +90,7 @@ console.log(animalsFiltered)
 // Display in the console : ['Cheval']
 // Elements from animalsFiltered with more than 5 characters
 console.log(animalsFilteredBis)*/
+
 
 /*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
 
@@ -107,8 +118,7 @@ console.log(animals.slice(-4, -2))
 
 // Display in the console : ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
 // The original array has not been changed.
-console.log(animals)*/ 
-
+console.log(animals)*/
 
 /*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
 
@@ -122,6 +132,7 @@ console.log(animals.pop())
 // Because the last value has been deleted
 console.log(animals.length)*/
 
+
 /*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
 
 // Display in the console : 6
@@ -134,11 +145,10 @@ console.log(animals.shift())
 // Because the first value has been deleted
 console.log(animals.length)*/
 
-/*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
+let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
 
 // Display true
 console.log(animals.includes('Ours'))
 
 // Display false because 'Lion' is at index 0 while we are searching from index 1
-console.log(animals.includes('Lion', 1))*/
-
+console.log(animals.includes('Lion', 1))
---
+++ 01_Studi/04_Front_End/03_Learn_Program_Js/14_Array_Construct_Croperties_Methods/03_Methods_Manip_Copy_Merge/01_Cours.js
@@ -0,0 +1,147 @@
+/*let numbers = [101, 0, 25, 9, 51]
+
+numbers.reverse()
+
+// Display [51, 9, 25, 0, 101]
+console.log(numbers)*/
+
+/*let colors = ['Bleu']
+
+// Display 3 in the console : the new array length (2 added values)
+console.log(colors.push('Blanc', 'Rouge'))
+
+// Display colors array with the 2 added values : ['Bleu', 'Blanc', 'Rouge']
+console.log(colors)*/
+
+/*let colors = ['Bleu']
+
+// Display 3 in the console : the new array length (2 added values)
+console.log(colors.unshift('Blanc', 'Rouge'))
+
+// Display colors array with the 2 added values : ['Blanc', 'Rouge', 'Bleu']
+console.log(colors)*/
+
+let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
+
+// Display ['Chien']
+// Because the deletion starts at index 2 and concerns only 1 item.
+console.log(animals.splice(2, 1))
+
+// Display ['Lion', 'Chat', Cheval', 'Tigre', 'Ours']
+// Without 'Chien' which has been deleted
+console.log(animals)
+
+// Display ['Tigre', 'Ours']
+// Because the deletion starts at index 3 of the modified array and concerns 2 items.
+console.log(animals.splice(3, 2, 'Abeille', 'Aigle', 'Lynx'))
+
+// Display ['Lion', 'Chat', Cheval', 'Abeille', 'Aigle', 'Lynx']
+// Without 'Tigre' and 'Ours' which has been deleted and replaced by 'Abeille', 'Aigle' and 'Lynx'.
+console.log(animals)
+
+// Display ['Abeille', 'Aigle']
+// Because the deletion starts at the third element from the end (-3) and concerns 2 elements.
+console.log(animals.splice(-3, 2, 'Faucon'))
+
+// Display ['Lion', 'Chat', Cheval', 'Faucon', 'Lynx']
+console.log(animals)
+
+// Display ['Chat', Cheval', 'Faucon', 'Lynx']
+// Because the deletion goes from index 1 to the end because no end value has been passed as a parameter.
+console.log(animals.splice(1))
+
+// Display ['Lion']
+console.log(animals)
+
+// Display []
+// Empty array because there is no deletion (the second parameter is 0)
+console.log(animals.splice(0, 0, 'Requin'))
+
+// Display ['Requin', 'Lion']
+console.log(animals)
+
+/*let animals = ['Chat', 'Lynx']
+let otherAnimals = ['Lion', 'Tigre']
+let otherAnimals2 = ['Requin', 'Aigle']
+
+let allAnimals = animals.concat(otherAnimals, otherAnimals2)
+
+// Display : ['Chat', 'Lynx', 'Lion', 'Tigre', 'Requin', 'Aigle']
+// Concatenation of animals, otherAnimals and otherAnimals2
+console.log(allAnimals)
+
+// Concat values
+let otherAnimals3 = otherAnimals.concat('Chien', 'Ours')
+
+// Display : ['Lion', 'Tigre', 'Chien', 'Ours']
+// Concatenation of otherAnimals and 'Chien', 'Ours'
+console.log(otherAnimals3)
+
+// Creation of a multi-dimensional array by concatenation
+let allAnimals2 = [].concat(
+  [animals],
+  [otherAnimals],
+  [otherAnimals2]
+)
+
+// Display : [['Chat', 'Lynx'], ['Lion', 'Tigre'], ['Requin', 'Aigle']]
+console.log(allAnimals2)*/
+
+/*let animals = ['Lion', 'Chat', 'Chien', 'Cheval', 'Tigre', 'Ours']
+
+// Display : Lion,Chat,Chien,Cheval,Tigre,Ours
+console.log(animals.join())
+
+// Display : Lion / Chat / Chien / Cheval / Tigre / Ours
+console.log(animals.join(' / '))*/
+
+
+/*let animals = [
+    'Zèbre',
+    ['Lion', 'Tigre'],
+    'Vache',
+    ['Chat', 'Chien'],
+    'Aigle'
+  ]
+  
+  let animalsFlat = animals.flat()
+  
+  // Display : ['Zèbre', 'Lion', 'Tigre', 'Vache', 'Chat', 'Chien', 'Aigle']
+  console.log(animalsFlat)
+  
+  let animals2 = [
+    'Chat',
+    [
+      'Lynx',
+      ['Aigle', 'Faucon'],
+      ['Ours', 'Zèbre']
+    ],
+    'Chien',
+    [
+      ['Lion', 'Tigre']
+    ],
+    'Cheval'
+  ]
+  
+  let animals2Flat1 = animals2.flat()
+  
+  // Display : 
+  // As the default depth is 1, only the arrays at the first depth level have been flattened.*/
+  /*
+  [
+    'Chat',
+    'Lynx',
+    ['Aigle', 'Faucon'],
+    ['Ours', 'Zèbre'],
+    'Chien',
+    ['Lion','Tigre'],
+    'Cheval'
+  ]
+  */
+  /*console.log(animals2Flat1)
+  
+  let animals2Flat2 = animals2.flat(2)
+  
+  // Display : ['Chat', 'Lynx', 'Aigle', 'Faucon', 'Ours', 'Zèbre', 'Chien', 'Lion', 'Tigre', 'Cheval']
+  // As the defined depth is 2, all arrays have been flattened.
+  console.log(animals2Flat2)*/
