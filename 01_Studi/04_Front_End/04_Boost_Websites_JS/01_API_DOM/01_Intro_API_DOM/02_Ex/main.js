2fc60e8b880a3f33957d4ab115ba4466277136b4
TahiKari <tahkar@outloo.fr>
2023-05-26 16:15:26 +0200

    Add cour 01_API_DOM/02_Ex

---
+++ 01_Studi/04_Front_End/04_Boost_Websites_JS/01_API_DOM/01_Intro_API_DOM/02_Ex/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    let body = document.body
+    console.log(body.innerHTML)
+    let dom = document.getElementById('dom')
+    console.log(dom.parentNode)
+})
