f1bc3d5ba93bfaa5681c5821e66ac97f4af0ab08
TAHIRI Karim <tahkar@outlook.fr>
2023-06-18 16:12:08 +0200

    change

--- 01_Studi/04_Front_End/03_Learn_Program_Js/01_Test.js
+++ 01_Studi/04_Front_End/03_Learn_Program_Js/01_Test.js
@@ -1,17 +1,2 @@
-class Event {
-  
-  constructor(titre, date, description) {
-    this.titre = titre;
-    this.date = new Date(date);
-    this.description = description;
-  }
-
-  affEvent() {
-    let dateFormated = this.date.toLocaleDateString("fr-FR"); //Nous formatons la date avec la locale française.
-    console.log(`-------- ${dateFormated} --------\nTitre de l’évènement : ${this.titre}\nDescription : ${this.description}`);
-  }
-}
-
-const event = new Event("rdv medical", "2023-12-10T14:30:00", "visite medicale");
-
-event.affEvent();
+const nombre = 45;
+console.log(typeof nombre);
