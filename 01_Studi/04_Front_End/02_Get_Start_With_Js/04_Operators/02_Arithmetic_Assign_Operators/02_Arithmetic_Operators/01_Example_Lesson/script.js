978b844f660c1f6ee6d85fdff167aa9cdac5d0d6
TAHIRI Karim <tahkar@outlook.fr>
2023-07-27 23:24:39 +0200

    Add lesson02_Get_Start_With_Js/04_Operatos(01 & 02

---
+++ 01_Studi/04_Front_End/02_Get_Start_With_Js/04_Operators/02_Arithmetic_Assign_Operators/02_Arithmetic_Operators/01_Example_Lesson/script.js
@@ -0,0 +1,83 @@
+// L’opérateur « + »
+// L’opérateur + permet simplement d’additionner 2 valeurs.
+/* 
+let a = 2;
+let b = 567.42;
+console.log(a + b); 
+*/
+// Dans ce cas, console.log(a + b) affichera dans la console : 569,42.
+
+console.log("------------------------------------");
+
+// L’opérateur « - »
+// L’opérateur - permet simplement de soustraire une valeur à une autre.
+/* 
+let a = 567.42;
+console.log(a - 25); 
+*/
+// Dans ce cas, console.log(a - 25) affichera dans la console : 542,42.
+
+console.log("------------------------------------");
+
+// L’opérateur « * »
+// L’opérateur * permet d’obtenir le produit de 2 nombres (multiplication).
+/* 
+let a = 2 * 4.5;
+console.log(a); 
+*/
+// Dans ce cas, console.log(a) affichera dans la console : 9.
+
+console.log("------------------------------------");
+
+// L’opérateur « / »
+// L’opérateur / permet de diviser un nombre par un autre nombre :
+/*
+console.log(58 / 2);
+*/
+// Dans ce cas, console.log(58/2) affichera dans la console : 29.
+
+console.log("------------------------------------");
+
+// L’opérateur « % »
+// L’opérateur % (qu’on appelle en général « modulo ») permet d’obtenir le reste d’une division euclidienne :
+/*
+let a = 90;
+let b = 11;
+console.log(a % b);
+*/
+// Dans ce cas, console.log(a % b) affichera dans la console : 2.
+// Pour bien comprendre, si on fait une division euclidienne avec 90 comme dividende et 11 comme diviseur, on obtient :
+// 90 = 11 x 8 + 2
+// On a donc un quotient égal à 8 et un reste égal à 2.
+
+console.log("------------------------------------");
+
+// L’opérateur « ** »
+// L’opérateur ** permet d’élever un nombre à une puissance. Par exemple, si on veut obtenir l’écriture décimale de 184, on fera :
+/*
+console.log(18 ** 4);
+*/
+// Dans ce cas, console.log(18 ** 4) affichera dans la console : 104 976.
+// 184 = 18 x 18 x 18 x18 = 104 976
+
+console.log("------------------------------------");
+
+// Les opérateurs « ++ » et « -- » 
+// L’opérateur ++ qu’on appelle « opérateur d’incrémentation » permet d’ajouter l’entier 1 à une valeur (on dit qu’il incrémente une variable).
+/*
+let a = 2;
+a ++;
+console.log(a);
+*/
+// Dans ce cas, console.log(a) affichera dans la console : 3.
+// En effet, l’incrémentation de a revient à écrire :
+/*
+a = a + 1;
+*/
+// L’opérateur -- est quant à lui appelé « opérateur de décrémentation ». Il fonctionne exactement comme l’opérateur d’incrémentation, mais permet en revanche de retirer 1 à une valeur :
+/*
+let a = 3;
+a --;
+console.log(a);
+*/
+// Dans ce cas, console.log(a) affichera dans la console : 2.
