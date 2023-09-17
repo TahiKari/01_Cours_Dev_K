d5b9c341126b3639f11c81e9b77d9fbfc8749d0e
TAHIRI Karim <tahkar@outlook.fr>
2023-09-17 08:09:35 +0200

    Add lesson 05_Boost_Websites_Js/03_HTML_Js_Forms_In_Out/03_Manip_Checkbox_Radio_Select

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/03_Manip_Checkbox_Radio_Select/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/03_Manip_Checkbox_Radio_Select/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,46 @@
-// Exemple 
+// Exemple 1
 /*
+var checkbox = document.getElementById('check')
 
+checkbox.addEventListener('change', () => {
+  alert(event.target.checked) // True si coché, sinon false 
+})
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 2
 /*
+var btnradio = document.getElementsByName('gender')
 
+for (var count=0; count<btnradio.length; count++) {
+    btnradio[count].addEventListener('change', (event) => {
+        alert(`Choix : ${event.target.value}`);
+    });
+}
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 3
 /*
-
+event.target.value = "Femme"
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 4
 /*
+var language = document.getElementById('language')
 
+language.addEventListener('change', (event) => {
+  alert(event.target.value)
+})
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 5
 /*
-
+event.target.value = "php"
 */
-
-console.log("-----------------------------------------------")
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,24 @@
-// Exemple 
-/*
+// Ma solution
+let checkbox = document.getElementById('consentement')
+let radio = document.getElementsByName('btn-radio')
+let select = document.getElementById('pays')
+let message = document.querySelector('#message')
 
-*/
+checkbox.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    alert('Vous acceptez de recevoir la newsletter')
+    for(item of radio) {
+      if (item.value == 'oui') {
+        item.checked = true
+      }
+    }
+  } else {
+    for(item of radio) {
+      item.checked = false
+    }
+  }
+})
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+select.addEventListener('change', (event) => {
+  message.innerText = `Vous avez indiqué être en ${event.target.value}`
+})
