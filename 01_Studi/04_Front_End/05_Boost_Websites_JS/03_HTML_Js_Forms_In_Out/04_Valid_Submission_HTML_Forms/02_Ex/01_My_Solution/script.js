207f7f809abf29bd43fba8540c00e0842e1d04be
TAHIRI Karim <tahkar@outlook.fr>
2023-09-17 08:10:37 +0200

    Correctif error folder

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/02_Ex/01_My_Solution/script.js
@@ -1,24 +1,34 @@
-// Ma solution
-let checkbox = document.getElementById('consentement')
-let radio = document.getElementsByName('btn-radio')
-let select = document.getElementById('pays')
-let message = document.querySelector('#message')
+// Exemple 
+/*
 
-checkbox.addEventListener('change', (event) => {
-  if (event.target.checked) {
-    alert('Vous acceptez de recevoir la newsletter')
-    for(item of radio) {
-      if (item.value == 'oui') {
-        item.checked = true
-      }
-    }
-  } else {
-    for(item of radio) {
-      item.checked = false
-    }
-  }
-})
+*/
 
-select.addEventListener('change', (event) => {
-  message.innerText = `Vous avez indiqué être en ${event.target.value}`
-})
+console.log("-----------------------------------------------")
+
+// Exemple 
+/*
+
+*/
+
+console.log("-----------------------------------------------")
+
+// Exemple 
+/*
+
+*/
+
+console.log("-----------------------------------------------")
+
+// Exemple 
+/*
+
+*/
+
+console.log("-----------------------------------------------")
+
+// Exemple 
+/*
+
+*/
+
+console.log("-----------------------------------------------")
