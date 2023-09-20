b5df10794f63887398afdbac90bbcac84eb8dad7
TAHIRI Karim <tahkar@outlook.fr>
2023-09-20 08:20:29 +0200

    Add lesson 05_Boost_Websites_Js/03_Html_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms and add lesson 05_Boost_Websites_Js/03_Html_Js_Forms_In_Out/05_Ex_Defi

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,66 @@
-// Exemple 
+// Exemple 1
 /*
-
+form.addEventListener('submit', (event) => {
+ // ici nos traitements sur les données du formulaire
+ event.preventDefault(); // Annule l'envoi des données
+})
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 2
 /*
+var form = document.querySelector('form')
 
+// Nombre d'éléments du formulaire
+console.log(`Choix : ${form.elements.length}`);
+
+// Nom du 1er élément
+console.log(`Choix : ${form.elements[0].name}`);
+
+// Type du second élément
+console.log(`Choix : ${form.elements[1].type}`);
+
+// Valeur du second élément
+console.log(`Choix : ${form.elements[1].value}`);
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 3
 /*
+var form = document.querySelector('form')
 
+form.addEventListener('submit', (event => {
+  for(var countElement=0; countElement<form.elements.length; countElement++) {
+    console.log(`Choix : ${form.elements[countElement].name}`);
+  }
+  event.preventDefault()
+}))
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 4
 /*
+var mytxt = document.getElementById('my-txt')
+var error = document.getElementById('error')
 
+mytxt.addEventListener('input', (event) => {
+  var mytxtValue = event.target.value
+  if (mytxtValue.length < 6) {
+    error.innerText = 'Le texte doit comporter au moins 6 caractères ...'
+  } else {
+    error.innerText = 'Texte validé !'
+  }
+})
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 5
 /*
-
+mytxt.addEventListener('blur', (event) => { ... })
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/03_HTML_Js_Forms_In_Out/04_Valid_Submission_HTML_Forms/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,31 @@
-// Exemple 
-/*
+// Ma solution
 
-*/
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let form = document.querySelector('form')
+let error = document.getElementById('error')
 
-console.log("-----------------------------------------------")
+var regexEmail = /^([0-9a-zA-Z].*?@([0-9a-zA-Z].*\.\w{2,4}))$/
 
-// Exemple 
-/*
+email.addEventListener('input', event => {
+  if (!regexEmail.test(event.target.value)) {
+    error.innerText = "Le format de l'email est incorrect"
+  } else {
+    error.innerText = ''
+  }
+})
 
-*/
+password.addEventListener('input', event => {
+  if (event.target.value.length < 8) {
+    error.innerText = 'le mot de passe doit contenir au moins 8 caractères'
+  } else {
+    error.innerText = ''
+  }
+})
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+form.addEventListener('submit', event => {
+  if (error.innerText !== '') {
+    event.preventDefault()
+    alert("Le formulaire contient des erreurs et n'a pas été envoyé")
+  }
+})
