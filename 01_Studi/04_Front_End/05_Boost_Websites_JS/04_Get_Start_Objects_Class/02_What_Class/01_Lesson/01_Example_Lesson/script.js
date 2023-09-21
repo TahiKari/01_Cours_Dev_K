67c5942d9ce0eeffd5c9362cc774073a13ab009d
TAHIRI Karim <tahkar@outlook.fr>
2023-09-21 07:10:48 +0200

    Add lesson 05_Boost_Websites_Js/04_Get_Start_Objects_Class/01_Context and 05_Boost_Websites_Js/04_Get_Start_Objects_Class/02_What_Class

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/02_What_Class/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/02_What_Class/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,69 @@
-// Exemple 
+// Exemple 1
 /*
+// Fonction décrivant une voiture
+function Voiture() {
+  this.marque= 'Peugeot'
+}
 
+// Fonction décrivant un conducteur
+function Conducteur() {
+  this.name= 'Philippe'
+}
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 2
 /*
+class Voiture {
+  constructor() {
+    this.marque = 'Peugeot'
+  }
+}
 
+class Conducteur {
+  constructor() {
+    this.name= 'Philippe'
+  }
+}
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 3
 /*
-
+let voiture = new Voiture()
+let conducteur = new Conducteur()
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 4
 /*
-
+let voiture1 = new Voiture()
+let voiture2 = new Voiture()
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 5
 /*
-
+let personne1 = new Object({
+  nom: 'Doe',
+  prenom: 'John'
+})
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
+
+// Exemple 6
+/*
+let personne1 = new Object({
+  nom: 'Doe',
+  prenom: 'John'
+})
+
+let personne2 = Object.create(personne1)
+personne2.nom = 'Doe'
+personne2.prenom = 'Jane'
+*/
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/02_What_Class/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/02_What_Class/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,28 @@
-// Exemple 
-/*
+// Ma solution
 
-*/
+// Version ES5 class Personne
 
-console.log("-----------------------------------------------")
+function Personne (nom, prenom, age, sexe, adresse) {
+  this.nom = nom
+  this.prenom = prenom
+  this.age = age
+  this.sexe = sexe
+  this.adresse = adresse
+}
 
-// Exemple 
-/*
+// Version ES6 class Humain
 
-*/
+class Humain {
+  constructor (nom, prenom, age, sexe, adresse) {
+    this.nom = nom
+    this.prenom = prenom
+    this.age = age
+    this.sexe = sexe
+    this.adresse = adresse
+  }
+}
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+let personne1 = new Personne()
+console.log(personne1)
+let personne2 = new Humain()
+console.log(personne2)
