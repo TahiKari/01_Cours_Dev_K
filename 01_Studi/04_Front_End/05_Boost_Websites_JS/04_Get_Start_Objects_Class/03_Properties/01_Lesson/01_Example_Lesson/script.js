37a70ee529fd3106f875b39a70066b1e3a86e8e7
TAHIRI Karim <tahkar@outlook.fr>
2023-09-22 07:32:57 +0200

    05_Boost_Websites_Js/04_Get_Start_Objects_Class/03_Properties

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/03_Properties/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/03_Properties/01_Lesson/01_Example_Lesson/script.js
@@ -1,33 +1,84 @@
-// Exemple 
+// Exemple 1
 /*
+// Version ES5
+function Voiture() {
+  this.moteur // Accès à la propriété moteur dans la classe
+}
 
+let voiture = new Voiture();
+voiture.moteur // Accès à la propriété moteur en dehors de la classe
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 2
 /*
+class Voiture{
+  constructor() {
+    this.moteur
+  }
+}
 
+let voiture = new Voiture();
+voiture.moteur // Accès à la propriété moteur en dehors de la classe
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 3
 /*
+// Version ES5
+function Voiture(moteur, roues, carroserie) {
+  this.moteur = moteur
+  this.roues = roues
+  this.carroserie = carroserie
+  this.volant = 'sport'
+}
 
+let voiture = new Voiture('1.6L', 4, 'Berline')
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 4
 /*
+// Version ES6
+class Voiture{
+  constructor(moteur, roues, carroserie) {
+    this.moteur = moteur
+    this.roues = roues
+    this.carroserie = carroserie
+    this.volant = 'sport'
+  }
+}
 
+let voiture = new Voiture('1.8L', 4, 'Break')
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 5
 /*
+function Voiture(moteur, roues, carroserie) {
+  this.moteur = moteur
+  this.roues = roues
+  this.carroserie = carroserie
+  this.vitesse = 0
+
+  // la fonction affiche() est rattachée à l’objet voiture 
+	// grâce au mot-clé this.
+  this.affiche = function() {
+    console.log(this.moteur,this.roues, this.carroserie, this.volant)
+  }
+}
+
+// La propriété volant est partagée entre tous les objets Voiture.
+Voiture.prototype.volant = 'Sport'
+
+let voiture1 = new Voiture('1.6L', 4, 'Berline')
+let voiture2 = new Voiture('1.8L', 4, 'Break')
+voiture1.affiche() // 1.6L 4 Berline Sport
+voiture2.affiche() // 1.8L 4 Break Sport
 
 */
 
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/03_Properties/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/03_Properties/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,17 @@
-// Exemple 
-/*
+// Ma solution
+// Version ES6
+class Personne {
+  constructor (nom, prenom, age, sexe, adresse) {
+    this.nom = nom
+    this.prenom = prenom
+    this.age = age
+    this.sexe = sexe
+    this.adresse = adresse
+  }
+}
 
-*/
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+let personne = new Personne('Tah', 'kar', 30, 'Mâle', '539 Chemin De Bellevis')
+personne.age = 41
+personne.adresse = '314 Chemin De Bellevis'
+console.log(personne.nom, personne.prenom, personne.age, personne.sexe, personne.adresse)
