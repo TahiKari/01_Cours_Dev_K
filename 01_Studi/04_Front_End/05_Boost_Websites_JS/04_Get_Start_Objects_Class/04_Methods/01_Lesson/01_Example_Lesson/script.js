7b19c15c0021e248a7d5190b5d563564f0768da3
TAHIRI Karim <tahkar@outlook.fr>
2023-09-23 07:05:09 +0200

    Add lesson 05_Boost_Websites_Js/04_Get_Start_Objects_Class/04_Methods

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/04_Methods/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/04_Methods/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,95 @@
-// Exemple 
+// Exemple 1
 /*
+// Version ES5
+function Voiture(moteur, roues, carroserie) {
+  this.moteur = moteur
+  this.roues = roues
+  this.carroserie = carroserie
+  this.volant = 'sport'
+  this.vitesse = 0
 
+  // Méthode permettant à une voiture de rouler
+  this.accelere = function(vitesse) {
+    this.vitesse += vitesse
+    console.log(`Nous roulons à ${this.vitesse} Km/h.`)
+  }
+
+  // Méthode permettant d'afficher les caractéristiques d'une voiture
+  this.affiche = function() {
+    console.log(this.moteur,this.roues, this.carroserie, this.volant)
+  }
+}
+
+
+let voiture = new Voiture('1.6L', 4, 'Berline')
+voiture.affiche()
+voiture.accelere(50)
+*/
+/*
+// Version ES6
+class Voiture{
+  constructor(moteur, roues, carroserie) {
+    this.moteur = moteur
+    this.roues = roues
+    this.carroserie = carroserie
+    this.volant = 'sport'
+    this.vitesse = 0
+  }
+
+  accelere(vitesse) {
+    this.vitesse += vitesse
+    console.log(`Nous roulons à ${this.vitesse} Km/h.`)
+  }
+
+  affiche() {
+    console.log(this.moteur,this.roues, this.carroserie, this.volant)
+  }
+}
+
+let voiture = new Voiture('1.8L', 4, 'Break')
+voiture.affiche()
+voiture.accelere(80)
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 2
 /*
+// Objet voiture1
+{
+  moteur: '1.6L',
+  roues: 4,
+  carroserie: 'Berline',
+  volant: 'Sport',
+  vitesse: 0,
 
+  accelere: function(vitesse) {
+    this.vitesse = vitesse
+    console.log(`Nous roulons à ${this.vitesse} Km/h.`)
+  }
+}
+
+//Objet voiture2
+{
+  moteur: '1.8L',
+  roues: 4,
+  carroserie: 'Break',
+  volant: 'Sport',
+  vitesse: 0,
+
+  accelere: function(vitesse) {
+    this.vitesse = vitesse
+    console.log(`Nous roulons à ${this.vitesse} Km/h.`)
+  }
+}
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 3
 /*
-
+Voiture.prototype.accelere = function(vitesse) {
+  this.vitesse = vitesse
+  console.log(`Nous roulons à ${this.vitesse} Km/h.`)
+}
 */
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/04_Methods/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/04_Get_Start_Objects_Class/04_Methods/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,46 @@
-// Exemple 
-/*
+// Ma solution
+// Version ES6
+class Personne {
+  constructor (nom, prenom, age, sexe, adresse) {
+    this.nom = nom
+    this.prenom = prenom
+    this.age = age
+    this.sexe = sexe
+    this.adresse = adresse
+  }
 
-*/
+  identite () {
+    let identite = `${this.prenom.substring(0, 1)}. ${this.nom}`
+    console.log(identite)
+  }
 
-console.log("-----------------------------------------------")
+  gender () {
+    if (this.sexe === 'Mr') {
+      console.log(`Monsieur ${this.nom}`)
+    } else {
+      console.log(`Madame ${this.nom}`)
+    }
+  }
+}
 
-// Exemple 
-/*
+Personne.prototype.changeAge = function (age) {
+  this.age = age
+  console.log(`${this.prenom} a ${this.age} ans`)
+}
 
-*/
+let personne1 = new Personne('Doe', 'John', 31, 'M', 'Rue du JavaScript')
+let personne2 = new Personne(
+  'Doe',
+  'Ann',
+  28,
+  'F',
+  'Avenue des classes et objets'
+)
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+personne1.identite()
+personne2.identite()
+console.log('-----------------------------------')
+personne1.gender()
+personne2.gender()
+console.log('-----------------------------------')
+personne1.changeAge(41)
