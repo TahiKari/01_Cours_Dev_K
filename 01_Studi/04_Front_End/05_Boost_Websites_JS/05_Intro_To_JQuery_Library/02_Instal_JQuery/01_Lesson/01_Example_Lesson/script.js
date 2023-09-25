2fbfb69d573e0e0ddbae6f0af2c85b754100ebee
TAHIRI Karim <tahkar@outlook.fr>
2023-09-25 07:47:44 +0200

    Add lesson 05_Boost_Websites_Js/05_Intro_To_JQuery_Library/01_Context and 02_Install_JQuery

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/05_Intro_To_JQuery_Library/02_Instal_JQuery/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/05_Intro_To_JQuery_Library/02_Instal_JQuery/01_Lesson/01_Example_Lesson/script.js
@@ -1,11 +1,27 @@
-// Exemple 
-/*
+// Exemple 1
+/*!
+ * jQuery JavaScript Library v3.4.1
+ * https://jquery.com/
+ *
+ * Includes Sizzle.js
+ * https://sizzlejs.com/
+ *
+ * Copyright JS Foundation and other contributors
+ * Released under the MIT license
+ * https://jquery.org/license
+ *
+ * Date: 2019-05-01T21:04Z
+ */
+/*( function( global, factory ) {
 
+	"use strict";
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {}
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple
 /*
 
 */
