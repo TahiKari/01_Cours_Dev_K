69899c7430456f44da24ccac289e4fde759831ef
TAHIRI Karim <tahkar@outlook.fr>
2023-10-01 07:23:19 +0200

    Add lesson 05_Boost_Websites_Js/06_JQuery_Basics/02_Selectors

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/06_JQuery_Basics/02_ Selectors/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/06_JQuery_Basics/02_ Selectors/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,64 @@
-// Exemple 
+// Exemple 1
+/*
+$('#bases') // Sélectionne <div id="bases">
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 2
+/*
+// Sélection du paragraphe contenu dans la div.
+$('div p')) // Sélection de la balise div puis de la balise p.
+$('#content p')) // Sélection de l'élément portant l'id #content puis de la balise p.
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 3
+/*
+$('#content:first-child') // Sélectionnera également le paragraphe, 1er enfant de la div #content.
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 4
+/*
+$('h1, h2, h3, .titre') // Sélectionnera les balises h1, h2 et h3 ainsi que les éléments ayant la classe .titre
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 5
+/*
+$('#content p').text('Bonjour tout le monde !')
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 6
+/*
+$('#content p').html(function() {
+    $(this).text('Bonjour tout le monde !')
+})
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 7
+/*
+$('#content')
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 8
+/*
+$('.content')
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 9
 /*
 
 */
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
