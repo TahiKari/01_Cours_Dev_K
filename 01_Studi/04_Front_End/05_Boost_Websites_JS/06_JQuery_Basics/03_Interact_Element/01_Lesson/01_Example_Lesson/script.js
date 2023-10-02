4f1c28097a184d0018ed44cc5c6ff060e3bcaff3
TAHIRI Karim <tahkar@outlook.fr>
2023-10-02 09:14:25 +0200

    Add lesson 05_Boost_Websites_Js/06_JQuery_Basics/03_Interact_Element

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/06_JQuery_Basics/03_Interact_Element/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/06_JQuery_Basics/03_Interact_Element/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,84 @@
-// Exemple 
+// Exemple 1
 /*
-
+$(document).ready(() => {
+    console.log($('#content').html())
+})
+*/
+/*
+// Affichera :
+  <h3>Présentation de jQuery</h3>
+  <p>Enfant de #content</p>
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 2
 /*
-
+$('#content').html('<div>Nouveau contenu</div>')
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 3
 /*
-
+$(document).ready(() => {
+    $('h3').css('color', 'blue')
+})
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 4
 /*
-
+$(document).ready(() => {
+    $('#list li').each(function() {
+        console.log($(this).text())
+    })
+})
+*/
+/*
+// Affichera :
+PHP
+HTML
+CSS
+jQuery
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 5
 /*
+$(document).ready(() => {
+let element = $( "#content" );
 
+$.data(element, "languages", ['PHP', 'HTML', 'CSS', 'jQuery']);
+
+$('#list li').each(function(index) {
+$(this).text($.data(element, "languages")[index])
+})
+});
+*/
+
+/*
+$(document).ready(() => {
+let element = $("#content");
+
+$.data(element, "languages", {
+0: "PHP",
+1: "HTML",
+2: "CSS",
+3: "jQuery"
+});
+
+$('#list li').each(function(index) {
+$(this).text($.data(element, "languages")[index])
+})
+});
 */
 
 console.log("-----------------------------------------------")
+
+// Exemple 6
+/*
+$( "select#foo" ).val(); 
+*/
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/06_JQuery_Basics/03_Interact_Element/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/06_JQuery_Basics/03_Interact_Element/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,19 @@
-// Exemple 
-/*
+// Ma solution
 
-*/
+$(document).ready(() => {
+  // On insère dans #introduction-copy le contenu lu dans #introduction
+  $('#introduction-copy').html($('#introduction').html())
 
-console.log("-----------------------------------------------")
+  let goals = $('#goals')
 
-// Exemple 
-/*
+  $.data(goals, 'goals', {
+    0: 'Récupérer le code HTML',
+    1: 'Modifier le CSS',
+    2: "Parcourir une liste d'éléments",
+    3: 'Associer des données à un élément'
+  })
 
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+  $('#goals li').each(function (index) {
+    $(this).text($.data(goals, 'goals')[index])
+  })
+})
