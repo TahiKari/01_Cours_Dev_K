02d41652b29c43c98f583a7fbd9b28fbcfbd163d
TAHIRI Karim <tahkar@outlook.fr>
2023-10-07 08:38:31 +0200

    Add lesson 05_Boost_Websites_Js/07_Ajax_Program/01_Context and add lesson 05_Boost_Websites_Js/07_Ajax_Program/02_XMLHttpRequest

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/07_AJAX_Program/02_XMLHttpRequest/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/07_AJAX_Program/02_XMLHttpRequest/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,95 @@
-// Exemple 
+// Exemple 1
 /*
-
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://reqres.in/api/users?page=1');
+xhr.send();
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 2
 /*
-
+xhr.open('get', 'https://reqres.in/api/users?page=1', false);
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 3
 /*
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://reqres.in/api/users');
 
+// readystatechange sera appelée chaque fois que le readyState changera. Ce qui permettra de générer un callback.
+xhr.addEventListener('readystatechange', function() {
+  // readyState indiquera l'état de la requête de l'objet XMLHttpRequest.
+    console.log("Current XHR state = " + xhr.readyState);
+});
+
+xhr.send();
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 4
 /*
+const xhr = new XMLHttpRequest();
+xhr.open('GET', "https://reqres.in/api/users");
 
+xhr.addEventListener('readystatechange', function() {
+    if(xhr.readyState === 4) {
+        console.log("Response = " + xhr.response);
+    };
+});
+
+xhr.send();
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 5
 /*
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://reqres.in/api/users?page=2');
 
+xhr.addEventListener('readystatechange', function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        console.log("Response = " + xhr.response);
+    };
+});
+
+xhr.send();
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
+
+// Exemple 6
+/*
+Response = {"page":2,"per_page":6,"total":12,"total_pages":2,"data":[{"id":7,"email":"michael.lawson@reqres.in","first_name":"Michael","last_name":"Lawson","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"},{"id":8,"email":"lindsay.ferguson@reqres.in","first_name":"Lindsay","last_name":"Ferguson","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"},{"id":9,"email":"tobias.funke@reqres.in","first_name":"Tobias","last_name":"Funke","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"},{"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"},{"id":11,"email":"george.edwards@reqres.in","first_name":"George","last_name":"Edwards","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"},{"id":12,"email":"rachel.howell@reqres.in","first_name":"Rachel","last_name":"Howell","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"}],"ad":{"company":"StatusCode Weekly","url":"http://statuscode.org/","text":"A weekly newsletter focusing on software development, infrastructure, the server, performance, and the stack end of things."}}
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 7
+/*
+const json = '{"page": 2, "per_page": 6}'; // C'est la représentation d'un objet JavaScript en JSON
+const object = JSON.parse(json);
+
+console.log(object); // Affiche {page: 2, per_page: 6}
+console.log(object.page); // Affiche 2
+*/
+
+console.log('-----------------------------------------------')
+
+// Exemple 8
+/*
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://reqres.in/api/users?page=2');
+
+xhr.addEventListener('readystatechange', function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+         const data = JSON.parse(xhr.response);
+    };
+});
+
+xhr.send();
+*/
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/07_AJAX_Program/02_XMLHttpRequest/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/07_AJAX_Program/02_XMLHttpRequest/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,16 @@
-// Exemple 
-/*
+// Ma solution
 
-*/
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://reqres.in/api/users?page=1')
 
-console.log("-----------------------------------------------")
+xhr.addEventListener('readystatechange', () => {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const data = JSON.parse(xhr.response).data
+    data.forEach(user => {
+      user.name = `${user.first_name} ${user.last_name}`
+    })
+    console.log(data)
+  }
+})
 
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+xhr.send()
