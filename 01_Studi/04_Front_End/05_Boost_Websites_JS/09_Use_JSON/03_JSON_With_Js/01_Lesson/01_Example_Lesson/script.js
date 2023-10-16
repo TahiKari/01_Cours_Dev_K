eadeea7aab2e5444e86a1ecacb531cc81241c807
TAHKAR <tahkar@outlook.fr>
2023-10-16 18:56:21 +0200

    Add lesson 08_Boost_Websites_Js/09_Use_JSON/03_JSON_With_Js

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/09_Use_JSON/03_JSON_With_Js/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/09_Use_JSON/03_JSON_With_Js/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,124 @@
-// Exemple 
+// Exemple 1
 /*
+const fields = [
+  {
+    id: 2,
+    label: "Terrain de foot",
+    area: 200
+  },
+  {
+    id: 5,
+    label: "Terrain de rugby",
+    area: null
+  }
+]
 
+const data = {
+  stadium: {
+    id: 18,
+    label: "Stade de France",
+    fields: fields
+  }
+}
+
+const json = JSON.stringify(data);
+
+console.log(json);
 */
 
-console.log("-----------------------------------------------")
-
-// Exemple 
 /*
+const object = {
+  today: new Date(),
+};
 
+const jsonObject = JSON.stringify(object);
+
+console.log(object.today);
+console.log(jsonObject);
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// Exemple 2
 /*
+const json = '{"stadium":{"id":18,"label":"Stade de France","fields":[{"id":2,"label":"Terrain de foot","area":200},{"id":5,"label":"Terrain de rugby","area":null}]}}';
 
+const data = JSON.parse(json);
+
+console.log(data.stadium.id);
 */
 
-console.log("-----------------------------------------------")
-
-// Exemple 
 /*
+try {
+  const json = '{"stadium:{"id":18,"label":"Stade de France","fields":[{"id":2,"label":"Terrain de foot","area":200},{"id":5,"label":"Terrain de rugby","area":null}]}}';
 
+  const data = JSON.parse(json);
+
+  console.log(data.stadium.id);
+} catch(error) {
+  console.log(error);
+}
+*/
+/*
+SyntaxError: Unexpected token i in JSON at position 12
+    at JSON.parse (<anonymous>)
+    at <anonymous>:4:21
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
+// exemple 3
+// Sérialisation
 /*
+const fields = [
+  {
+    id: 2,
+    label: "Terrain de foot",
+    area: 200
+  },
+  {
+    id: 5,
+    label: "Terrain de rugby",
+    area: null
+  },
+]
 
+const data = {
+  stadium: {
+    id: 18,
+    label: "Stade de france",
+    fields: fields
+  }
+}
+
+const json = JSON.stringify(data)
+
+console.log(json)
 */
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
+
+// exemple 4
+// Désérialisation
+/*
+const json = '{"stadium":{"id":18,"label":"Stade de france","fields":[{"id":2,"label":"Terrain de foot","area":200},{"id":5,"label":"Terrain de rugby","area":null}]}}'
+
+const data = JSON.parse(json)
+
+console.log(data.stadium.fields[0].label)
+*/
+
+console.log('-----------------------------------------------')
+
+// exemple 5
+// En cas d'erreur
+try {
+  const json = '{"stadium":{"id":18,"label":"Stade de france","fields":[{"id":2,"label":"Terrain de foot","area":200},{"id":5,"label":"Terrain de rugby""area":null}]}}'
+
+const data = JSON.parse(json)
+
+console.log(data.stadium.fields[0].label)
+} catch (error) {
+  console.log(error)
+}
+
