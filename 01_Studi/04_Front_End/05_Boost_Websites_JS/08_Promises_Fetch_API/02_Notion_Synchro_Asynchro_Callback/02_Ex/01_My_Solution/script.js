beccb5962e7ff7a8e0629467b60e4e2c63c5b2ba
TAHKAR <tahkar@outlook.fr>
2023-10-16 08:30:04 +0200

    Add lesson maj reinstall githuh  in vscode

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/08_Promises_Fetch_API/02_Notion_Synchro_Asynchro_Callback/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/08_Promises_Fetch_API/02_Notion_Synchro_Asynchro_Callback/02_Ex/01_My_Solution/script.js
@@ -1,10 +1,12 @@
 // Ma solution
 
 let mySandwich = (param1, param2, callback) => {
-  alert('Je mange un sandwich à : ' + param1 + ', ' + param2)
-  callback()
-}
+  alert("Je mange un sandwich à : " + param1 + ", " + param2);
+  callback();
+};
 
-const endSandwich = () => { alert('Fini de manger!')  }
+const endSandwich = () => {
+  alert("Fini de manger!");
+};
 
-mySandwich('jambon', 'fromage', endSandwich); 
+mySandwich("jambon", "fromage", endSandwich);
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/09_Use_JSON/02_Syntax/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/09_Use_JSON/02_Syntax/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,37 @@
-// Exemple 
+// Exemple 1
 /*
-
+{
+  "id": 10,
+  "type": "Etudiant",
+  "classes": ["Informatique", "Algorithmie"],
+  "fullTime": true,
+  "person": {
+    "id": 3,
+    "fullName": "John Doe",
+    "phone": null
+  }
+}
 */
 
-console.log("-----------------------------------------------")
+console.log("-----------------------------------------------");
 
-// Exemple 
+// Exemple 2
 /*
-
+{
+  "person": {
+    "adresses": {
+      "id": 45,
+      "data": [
+        {
+          "id": 89,
+          "city": "Paris"
+        },
+        {
+          "id": 14,
+          "city": "Lyon"
+        }
+      ]
+    }  
+  }
+}
 */
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
