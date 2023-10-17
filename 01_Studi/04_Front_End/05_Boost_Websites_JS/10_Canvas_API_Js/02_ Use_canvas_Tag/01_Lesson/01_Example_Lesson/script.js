38721a5d50a98e074f434025d6490297b8f36ec8
TAHKAR <tahkar@outlook.fr>
2023-10-17 10:19:37 +0200

    Add lesson 05_Boost_Websites_Js/10_Canvas_API_Js/01_Context and add lesson 05_Boost_Websites_Js/10_Canvas_API_Js/02_Use_Canvas_Tag

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/02_ Use_canvas_Tag/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/02_ Use_canvas_Tag/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,21 @@
-// Exemple 
+// Exemple 1
 /*
-
+// élément html canvas
+const canvas = document.getElementById('myFirstCanvas') ;
+// initialisation du contexte
+const ctx = canvas.getContext('2d') ;
 */
 
 console.log("-----------------------------------------------")
 
-// Exemple 
+// Exemple 2
 /*
-
+const canvas = document.getElementById('myFirstCanvas');
+let ctx;
+if (canvas.getContext) {
+  ctx = canvas.getContext('2d') ;
+} else {
+  // code pour les anciens navigateurs
+}
 */
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/02_ Use_canvas_Tag/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/02_ Use_canvas_Tag/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,14 @@
-// Exemple 
-/*
+// Ma solution
+// déclaration du canvas
+const canvas = document.getElementById('redSquare')
 
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+let ctx
+if (canvas.getContext) {
+  ctx = canvas.getContext('2d')
+  ctx.fillStyle = 'red'
+  ctx.fillRect(0, 0, 150, 150)
+} else {
+  console.log('canevas non supporté')
+}
+ctx.fillStyle = 'red'
+ctx.fillRect(0, 0, 150, 150)
