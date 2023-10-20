c86d2f90f78341c87ccef49e4990ef7d84f7daf1
TAHKAR <tahkar@outlook.fr>
2023-10-20 11:33:52 +0200

    Add lesson 05_Boost_Websites_Js/10_Canvas_API_Js/06_ Apply_Transforms

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/06_ Apply_Transforms/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/06_ Apply_Transforms/01_Lesson/01_Example_Lesson/script.js
@@ -1,34 +1,136 @@
-// Exemple 
-/*
+// Exemple 1
+const canvas1 = document.getElementById('canvas1')
+let ctx1
+if (canvas1.getContext) {
+  ctx1 = canvas1.getContext('2d')
+  ctx1.fillStyle = 'red'
+  ctx1.fillRect(200, 30, 30, 30)
+  ctx1.save() // Sauvegarde l'atat
 
-*/
+  ctx1.fillStyle = 'blue'
+  ctx1.fillRect(240, 30, 30, 30)
+  ctx1.save() // Sauvegarde l'état
 
-console.log("-----------------------------------------------")
+  ctx1.fillStyle = 'yellow'
+  ctx1.fillRect(280, 30, 30, 30)
 
-// Exemple 
-/*
+  ctx1.restore() // restaure le dernier état sauvegardé et efface la sauvegarde
+  ctx1.fillRect(320, 30, 30, 30)
 
-*/
+  ctx1.restore() // restaure le premier état sauvegardé et efface la sauvegarde
+  ctx1.fillRect(360, 30, 30, 30)
+} else {
+  // code pour les anciens navigateurs
+}
 
-console.log("-----------------------------------------------")
+console.log('-----------------------------------------------')
 
-// Exemple 
-/*
+// Exemple 2
+const canvas2 = document.getElementById('canvas2')
+let ctx2
+if (canvas2.getContext) {
+  ctx2 = canvas2.getContext('2d')
+  ctx2.fillStyle = 'red'
+  ctx2.fillRect(230, 15, 50, 50)
 
-*/
+  // On déplace le point d'origine du canevas
+  ctx2.translate(320, 60)
 
-console.log("-----------------------------------------------")
+  ctx2.fillStyle = 'blue'
+  ctx2.fillRect(10, 15, 50, 50)
+}
 
-// Exemple 
-/*
+console.log('-----------------------------------------------')
 
-*/
+// Exemple 3
+const canvas3 = document.getElementById('canvas3')
+let ctx3
 
-console.log("-----------------------------------------------")
+if (canvas3.getContext) {
+  ctx3 = canvas3.getContext('2d')
+  // Rotation
+  ctx3.rotate(Math.PI / 2)
+  ctx3.font = '30px Arial'
+  ctx3.fillText('Hello world', 15, -290)
+}
 
-// Exemple 
-/*
+console.log('-----------------------------------------------')
 
-*/
+// Exemple 4
+const canvas4 = document.getElementById('canvas4')
+let ctx4
+if (canvas4.getContext) {
+  ctx4 = canvas4.getContext('2d')
 
-console.log("-----------------------------------------------")
+  // Déplacement du point d'origine
+  ctx4.translate(300, 75)
+
+  // cercle de l'horloge
+  ctx4.beginPath()
+  ctx4.arc(0, 0, 50, 0, 2 * Math.PI)
+  ctx4.stroke()
+
+  // Mise en place des heures
+  ctx4.textAlign = 'center'
+  ctx4.fillText('12', 0, -40)
+  for (let i = 1; i < 12; i++) {
+    ctx4.rotate(Math.PI / 6)
+    ctx4.fillText(`${i}`, 0, -40)
+  }
+}
+
+console.log('-----------------------------------------------')
+
+// Exemple 5
+const canvas5 = document.getElementById('canvas5');
+let ctx5;
+if (canvas5.getContext) {
+  ctx5 = canvas5.getContext('2d') ;
+  ctx5.fillStyle = 'red'
+  ctx5.fillRect(200, 30, 30, 30)
+  ctx5.save()
+
+  ctx5.fillStyle = 'blue'
+  ctx5.transform(1, 0, 0, 1, 40, 0) // déplace de 40px sur l'axe des x
+  ctx5.fillRect(200, 30, 30, 30)
+  ctx5.save()
+
+  ctx5.fillStyle = 'yellow'
+  ctx5.transform(1, 0, 0, 1, 40, 0) // déplace de 40px sur l'axe des x
+  ctx5.fillRect(200, 30, 30, 30)
+
+  ctx5.restore()
+  ctx5.transform(1, 0, 0, 1, 80, 0) // déplace de 80px sur l'axe des x à partir du dernier état sauvegardé
+  ctx5.fillRect(200, 30, 30, 30)
+
+  ctx5.restore()
+  ctx5.transform(1, 0, 0, 1, 160, 0) // déplace de 160px sur l'axe des x à partir du premier état sauvegardé
+  ctx5.fillRect(200, 30, 30, 30)
+
+} else {
+  // code pour les anciens navigateurs
+}
+
+console.log('-----------------------------------------------')
+
+// Exemple 6
+
+const canvas6 = document.getElementById('canvas6')
+let ctx6
+if (canvas6.getContext) {
+  ctx6 = canvas6.getContext('2d')
+
+  ctx6.fillStyle = 'lightblue'
+  ctx6.fillRect(150, 120, 100, 100)
+
+  ctx6.translate(100, 0) // Déplace les élément déclarer ci dessous 
+
+  ctx6.fillStyle = 'lightpink'
+  ctx6.fillRect(150, 230, 100, 100)
+
+  ctx6.fillStyle = 'lightgreen'
+  ctx6.fillRect(150, 340, 100, 100)
+
+  ctx6.rotate(180 * Math.PI / 420)
+  ctx6.fillRect(300, 10, 100, 100)
+}
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/06_ Apply_Transforms/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/06_ Apply_Transforms/02_Ex/01_My_Solution/script.js
@@ -1,34 +1,33 @@
-// Exemple 
-/*
+// Ma solution
 
-*/
+const canvas = document.getElementById('hour')
+let ctx
+if (canvas.getContext) {
+  ctx = canvas.getContext('2d')
 
-console.log("-----------------------------------------------")
+  // Déplacement du point d'origine
+  ctx.translate(125, 115)
 
-// Exemple 
-/*
+  // cercle de l'horloge
+  ctx.beginPath()
+  ctx.arc(0, 0, 50, 0, 2 * Math.PI)
+  ctx.stroke()
 
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+  // Mise en place des heures
+  // On donne à la police une taille de 15% par rapport à la taille du rayon de l'horloge donc on multiplie le rayon par 0.15.
+  ctx.font = 50 * 0.15 + 'px arial'
+  ctx.textBaseline = 'middle'
+  ctx.textAlign = 'center'
+  for (num = 1; num < 13; num++) {
+    ang = (num * Math.PI) / 6
+    ctx.rotate(ang)
+    ctx.translate(0, -45) // +45 pour revenir au centre
+    ctx.rotate(-ang)
+    ctx.fillText(num.toString(), 0, 0)
+    ctx.rotate(ang)
+    ctx.translate(0, 50 * (Math.PI / 3 - 0.15))
+    ctx.rotate(-ang)
+  }
+} else {
+  // code ancien navigateur
+}
