537d032c161560a4c2c872b87647367a59cbfe61
TAHKAR <tahkar@outlook.fr>
2023-10-21 09:16:04 +0200

    Add lesson 05_Boost_Websites_Js/10_Canvas_PAI_Js/07_Ex_Defi and add lesson 05_Boost_Websites_Js/11_Intro_3D_Js_Three_Js/01_Context and add lesson 05_Boost_Websites_Js/11_Intro_3D_Js_Three_Js/02_WebGl_Three_Js

--- 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/08_Ex_Defi/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/10_Canvas_API_Js/08_Ex_Defi/01_My_Solution/script.js
@@ -1,34 +1,63 @@
-// Exemple 
-/*
+// Ma solution
 
-*/
+// fonction qui teste l'image
+const checkImageExists = (imageUrl, callBack) => {
+  const imageData = new Image();
+  imageData.onload = () => {
+    callBack(true);
+  };
+  imageData.onerror = () => {
+    callBack(false);
+  };
+  imageData.src = imageUrl;
+}
 
-console.log("-----------------------------------------------")
+// fonction de création du badge
+const createBadge = (canvas, urlImg = null) => {
+  let sizeCanvas = canvas.getAttribute('width');
+  sizeCanvas === '0' || sizeCanvas === null ? sizeCanvas = 150 : sizeCanvas
+  canvas.setAttribute('height', sizeCanvas)
+  canvas.setAttribute('width', sizeCanvas)
+  let ctx;
+  if (canvas.getContext) {
+    ctx = canvas.getContext('2d') ;
+    // On verifie si l'image existe
+    checkImageExists(urlImg, (imageExit) => {
+      if (imageExit) {
+        // création du badge avec image
+        const img = new Image();
+        img.addEventListener('load', () => {
+          const newWidth = img.width * sizeCanvas / img.height
+          ctx.drawImage(img, 0, 0, newWidth, sizeCanvas);
+          ctx.globalCompositeOperation='destination-in';
+          ctx.beginPath();
+          ctx.arc(sizeCanvas/2, sizeCanvas/2, sizeCanvas/2, 0, 2 * Math.PI);
+          ctx.fill();
+          ctx.globalCompositeOperation='source-over';
+        })
+        img.src = urlImg;
+      } else {
+        // création du badge avec texte
+        ctx.fillStyle = 'grey'
+        ctx.beginPath();
+        ctx.arc(sizeCanvas/2, sizeCanvas/2, sizeCanvas/2, 0, 2 * Math.PI);
+        ctx.fill();
 
-// Exemple 
-/*
+        const sizeText = sizeCanvas / 2
+        ctx.fillStyle = 'orange'
+        ctx.textAlign = 'center'
+        ctx.textBaseline = 'middle';
+        ctx.font = `${sizeText}px Arial`;
+        // On ajuste la position du texte par rapport à la taille du canvas
+        ctx.fillText('JD', sizeCanvas/2, sizeCanvas/2);
+      }
+    })
+  }
+}
 
-*/
+const canvas1 = document.getElementById('badgeWithImage');
+const img = "https://fotomelia.com/wp-content/uploads/edd/2015/05/main-fleur-paquerette-ciel-bleu-images-photos-gratuites.jpg";
+createBadge(canvas1, img);
 
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
-
-// Exemple 
-/*
-
-*/
-
-console.log("-----------------------------------------------")
+const canvas2 = document.getElementById('badgeWithText');
+createBadge(canvas2);
--- 01_Studi/04_Front_End/05_Boost_Websites_JS/11_Intro_3D_Js_Three_Js/02_WebGL_Three_Js/01_Lesson/01_Example_Lesson/script.js
+++ 01_Studi/04_Front_End/05_Boost_Websites_JS/11_Intro_3D_Js_Three_Js/02_WebGL_Three_Js/01_Lesson/01_Example_Lesson/script.js
@@ -1,14 +1,70 @@
-// Exemple 
-/*
-
-*/
+// Exemple 1
+let canvas1 = document.getElementById('canvas1');
+    let ctx1 = canvas1.getContext('2d'); // Récupération du contexte Canvas2D
+    ctx1.fillStyle = "#FF0000"; // Couleur de remplissage (rouge)
+    ctx1.fillRect(50, 50, 100, 75);  // Dessin d'un rectangle de 100x75 aux coordonnées (50,50)   
 
 console.log("-----------------------------------------------")
 
-// Exemple 
-/*
+// Exemple 2
+let canvas2 = document.getElementById('canvas2');
+    gl = canvas2.getContext('webgl'); // Récupération du contexte Canvas2D
 
-*/
+    let vertices = [
+        -0.5, 0.5, 0.0,
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0
+    ];
+    let indices = [0, 1, 2];
+
+    // Création de deux buffers de données : un pour les sommets et un pour les indices
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    let indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    // Création du shader des sommets et des fragments, puis du shader général
+    let vertCode =
+        'attribute vec3 coordinates;' +
+        'void main(void) {' +
+        ' gl_Position = vec4(coordinates, 1.0);' +
+        '}';
+
+    let vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vertCode);
+    gl.compileShader(vertShader);
+
+    let fragCode =
+        'void main(void) {' +
+        ' gl_FragColor = vec4(1, 0.0, 0.0, 1);' +
+        '}';
+    let fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShader, fragCode);
+    gl.compileShader(fragShader);
+
+    let shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertShader);
+    gl.attachShader(shaderProgram, fragShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    let coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coord);
+
+    // Dessin sur le canvas
+    gl.clearColor(1, 1, 1, 1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, canvas2.width, canvas2.height);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
 console.log("-----------------------------------------------")
 
