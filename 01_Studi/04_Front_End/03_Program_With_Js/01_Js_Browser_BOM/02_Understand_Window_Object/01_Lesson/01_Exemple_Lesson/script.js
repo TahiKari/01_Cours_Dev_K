d0afa252c4621b39d4a612435aac0fcf0164cb45
TAHKAR <tahkar@outlook.fr>
2023-10-24 18:33:19 +0200

    maj

--- 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object/01_Lesson/01_Exemple_Lesson/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object/01_Lesson/01_Exemple_Lesson/script.js
@@ -21,21 +21,25 @@ button.addEventListener('click', () => {
 console.log('----------------------------------------')
 
 // Exemple 3
-/*
-const button = document.getElementById("bouton");
+
+const button = document.getElementById('bouton')
 
 button.addEventListener('click', () => {
-  let fenetre = window.open("destination.html", 'destination', 'width=500, height=500');
+  let fenetre = window.open(
+    'destination.html',
+    'destination',
+    'width=500, height=500'
+  )
 
   fenetre.addEventListener('load', () => {
-    let button2 = fenetre.document.getElementById('bouton2');
+    let button2 = fenetre.document.getElementById('bouton2')
 
     button2.addEventListener('click', () => {
-      fenetre.close();
-    });
-  });
-});
-*/
+      fenetre.close()
+    })
+  })
+})
+
 
 console.log('----------------------------------------')
 
