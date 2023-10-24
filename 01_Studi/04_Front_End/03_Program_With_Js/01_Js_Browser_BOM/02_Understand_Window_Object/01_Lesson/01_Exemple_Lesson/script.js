6c3fe528896a7bbea764280f32cb8b0eb063180a
TAHKAR <tahkar@outlook.fr>
2023-10-24 13:43:32 +0200

    Add lesson 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object

--- 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object/01_Lesson/01_Exemple_Lesson/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object/01_Lesson/01_Exemple_Lesson/script.js
@@ -1,48 +1,73 @@
 // Exemple 1
 /*
+const button = document.getElementById("bouton");
 
+button.addEventListener('click', () => {
+  window.open("destination.html");
+});
 */
 
 console.log('----------------------------------------')
 
 // Exemple 2
 /*
+const button = document.getElementById("bouton");
 
+button.addEventListener('click', () => {
+  window.open("destination.html", 'destination', 'width=500, height=500');
+});
 */
 
 console.log('----------------------------------------')
 
 // Exemple 3
 /*
+const button = document.getElementById("bouton");
 
+button.addEventListener('click', () => {
+  let fenetre = window.open("destination.html", 'destination', 'width=500, height=500');
+
+  fenetre.addEventListener('load', () => {
+    let button2 = fenetre.document.getElementById('bouton2');
+
+    button2.addEventListener('click', () => {
+      fenetre.close();
+    });
+  });
+});
 */
 
 console.log('----------------------------------------')
 
 // Exemple 4
 /*
+const button = document.getElementById('bouton')
 
+button.addEventListener('click', () => {
+  window.alert('Message') //ou alert("Message")
+})
 */
 
 console.log('----------------------------------------')
 
 // Exemple 5
 /*
+const button = document.getElementById("bouton");
 
+button.addEventListener('click', () => {
+  let mot = window.prompt("Quel mot afficher ?");
+  window.alert(mot);
+});
 */
 
 console.log('----------------------------------------')
 
 // Exemple 6
 /*
+const button = document.getElementById("bouton");
 
+button.addEventListener('click', () => {
+  let mot = window.prompt("Quel mot afficher ?", "mot de votre choix");
+  window.alert(mot);
+});
 */
-
-console.log('----------------------------------------')
-
-// Exemple 7
-/*
-
-*/
-
-console.log('----------------------------------------')
--- 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object/02_Ex/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/01_Js_Browser_BOM/02_Understand_Window_Object/02_Ex/01_My_Solution/script.js
@@ -1,1 +1,71 @@
-// Ma solution
+// Ma solution 1
+
+/*
+window.addEventListener('load', () => {
+  alert('Bonjour, bienvenue sur mon site ! ')
+})
+*/
+
+console.log('------------------------------------')
+
+// Ma solution 2
+/*
+window.addEventListener('load', () => {
+  let userName = prompt('Entrez votre nom d\'utilisateur')
+  alert('Bienvenue ' + userName)
+})
+*/
+
+// Ou
+
+/*
+let nom = prompt("Quel est votre nom ?");
+console.log("Bienvenue " + nom);
+*/
+
+console.log('------------------------------------')
+
+// Ma solution 3
+/*
+let button = document.createElement('button')
+
+button.innerHTML = 'button'
+
+document.body.appendChild(button)
+
+//code
+button.addEventListener('click', () => {
+  window.open('https://www.google.com', 'google', 'width=500, height=500')
+})
+*/
+
+console.log('------------------------------------')
+
+// Ma solution 4
+/*
+let close = document.createElement('button');
+
+close.innerHTML = "close";
+
+document.body.appendChild(close);
+
+// code
+close.addEventListener('click', ()=> {
+  window.close()
+})
+console.log('------------------------------------')
+*/
+
+
+// Ma solution 5
+let open = document.createElement('button');
+
+open.innerHTML = "open";
+
+document.body.appendChild(open);
+
+// code
+let url = prompt("Entrez l'URL :")
+open.addEventListener('click', () => {
+  window.open(url)
+})
