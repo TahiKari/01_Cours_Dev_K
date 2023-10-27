6480dcab6fa80b53adfddd57550f64a6df6063da
TAHKAR <tahkar@outlook.fr>
2023-10-27 09:54:25 +0200

    Add lesson 04_Front_End/03_Program_With_Js/02_Asynchron_Function_Callback/05_Ex_Defi and add lesson 04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/01_Context and add lesson 04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/02_JSON_Format

--- 01_Studi/04_Front_End/03_Program_With_Js/02_Asynchron_Function_Callback/05_Ex_Defi/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/02_Asynchron_Function_Callback/05_Ex_Defi/01_My_Solution/script.js
@@ -1,1 +1,20 @@
 // Ma solution
+
+function wait() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 10000);
+  });
+}
+
+function chargedFile() {
+    console.log('Le fichier est chargé')
+}
+
+async function chargement(callbackFileWhenLoaded) {
+  await wait();
+  callbackFileWhenLoaded();
+}
+
+chargement(chargedFile);
+
+console.log("Plateforme de dépôt");
--- 01_Studi/04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/02_JSON_Format/01_Lesson/01_Exemple_Lesson/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/02_JSON_Format/01_Lesson/01_Exemple_Lesson/script.js
@@ -1,48 +1,121 @@
 // Exemple 1
 /*
-
+'{"name" :"Charles", "age":30, "car":null}'
 */
 
 console.log('----------------------------------------')
 
 // Exemple 2
 /*
-
+let jsonString = '{"name":"John", "age":30}';
+let obj = JSON.parse(jsonString);
+let personName = obj.name;
+let personAge = obj.age;
+console.log(personName);
+console.log(personAge);
 */
 
 console.log('----------------------------------------')
 
 // Exemple 3
 /*
-
+{"name" :"Charles"}
 */
 
 console.log('----------------------------------------')
 
 // Exemple 4
 /*
-
+{name:"Charles"}
 */
 
 console.log('----------------------------------------')
 
 // Exemple 5
 /*
+// Format JSON
+{"employees":[
+  { "firstName":"Charles", "lastName":"Dupont" },
+  { "firstName":"Marie", "lastName":"Leclerc" },
+  { "firstName":"Pierre", "lastName":"Lefranc" }
+]}
+*/
 
+/*
+// Format XML
+<employees>
+  <employee>
+    <firstName>Charles</firstName> <lastName>Dupont</lastName>
+  </employee>
+  <employee>
+    <firstName>Marie</firstName> <lastName>Leclerc</lastName>
+  </employee>
+  <employee>
+    <firstName>Pierre</firstName> <lastName>Lefranc</lastName>
+  </employee>
+</employees>
 */
 
 console.log('----------------------------------------')
 
-// Exemple 6
+// Exemple 6 de la vidéo
+// Partie 1 crée un objet depuis une chaîne JSON avec la fonction JSON.parse()
 /*
-
+const txt = '{"name": "Charles", "age": 31, "city": "Paris"}'
+const obj = JSON.parse(txt)
+document.getElementById('demo').innerHTML = obj.name + ", " + obj.age + ", " + obj.city + "."
 */
+// Partie 2 Analyser un tableau JSON avec JSON.parse()
+/*
+const array = '["Peugeot", "Fiat", "Citroen", "Bmw"]'
+const myArray = JSON.parse(array)
+document.getElementById('demo').innerHTML = myArray[2]
+*/
+
+// Partie Créer une chaîne JSON depuis un objet JavaScript avec JSON.stringify()
+/*
+const obj = {name:"Carles", age:35, city:"Paris"}
+const myJSON = JSON.stringify(obj)
+document.getElementById('demo').innerHTML = myJSON
+*/
+
+// Partie 4 Créer une chaîne JSON depuis un tableau JavaScript avec JSON.stringify()
+/*
+const array = ["Pierre", "Michel", "Charles", "Marie"]
+const myArray = JSON.stringify(array)
+document.getElementById("demo").innerHTML = myArray
+*/
+
+// Partie 5 Stocker et récupérer des données depuis localStorage
+//Stocker les données
+const myObj = {name: "Marie", age: 35, city:"Lyon"}
+const myJSON = JSON.stringify(myObj)
+localStorage.setItem("testJSON", myJSON)
+
+// Récupérer les données
+let text = localStorage.getItem("testJSON")
+let obj = JSON.parse(text)
+document.getElementById("demo").innerHTML = obj.name + ", " + obj.age + ", " + obj.city + "."
 
 console.log('----------------------------------------')
 
 // Exemple 7
 /*
-
+const xmlhttp = new XMLHttpRequest();
+			xmlhttp.onload = function() {
+  				const myObj = JSON.parse(this.responseText);
+ 				 document.getElementById("demo").innerHTML = myObj.name;
+			}
+			xmlhttp.open("GET",  "file.json");
+			xmlhttp.send();
 */
 
-console.log('----------------------------------------')
+/*
+const xmlhttp = new XMLHttpRequest();
+			xmlhttp.onload = function() {
+				const myArr = JSON.parse(this.responseText);
+				document.getElementById("demo").innerHTML = JSON.stringify(myArr, null, 2);
+			}
+			xmlhttp.open("GET", "car.json", true);
+			xmlhttp.send();
+*/
