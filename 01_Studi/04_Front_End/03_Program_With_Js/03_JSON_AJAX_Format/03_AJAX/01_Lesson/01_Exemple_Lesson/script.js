e100ac60170456e78caa0680049ccd268f3ce429
TAHKAR <tahkar@outlook.fr>
2023-10-28 11:00:52 +0200

    Add lesson 04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/03_AJAX and add lesson 04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/04_Esentia and add lesson 04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/05_Ex_Defi

--- 01_Studi/04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/03_AJAX/01_Lesson/01_Exemple_Lesson/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/03_AJAX/01_Lesson/01_Exemple_Lesson/script.js
@@ -1,48 +1,111 @@
 // Exemple 1
-/*
 
+//Création de la fonction
+/*  
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "demo.txt", true);
+    xhttp.send();
+  }
 */
 
-console.log('----------------------------------------')
+console.log("----------------------------------------");
 
 // Exemple 2
+// L’objet XMLHttpRequest
 /*
-
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("demo").innerHTML = this.responseText;
+  };
+  xhttp.open("GET", "demo.txt");
+  xhttp.send();
+}
 */
 
-console.log('----------------------------------------')
+// L’objet XMLHttpRequest avec un response XML
+/*
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function () {
+  const xmlDoc = this.responseXML;
+  const x = xmlDoc.getElementsByTagName("NAME");
+  let txt = "";
+  for (let i = 0; i < x.length; i++) {
+    txt = txt + x[i].childNodes[0].nodeValue + "<br>";
+  }
+  document.getElementById("demo").innerHTML = txt;
+};
+xhttp.open("GET", "car.xml");
+xhttp.send();
+*/
+
+//Fonction getAllResponseHeaders()
+// Cette fonction renvoie toutes les informations d'en-tête d'une ressource, comme la longueur, le type de serveur, le type de contenu, la dernière modification...
+/*
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function () {
+  document.getElementById("demo").innerHTML = this.getAllResponseHeaders();
+};
+xhttp.open("GET", "demo.txt");
+xhttp.send();
+*/
+console.log("----------------------------------------");
 
 // Exemple 3
-/*
+function loadDoc() {
+    const xhttp = new XMLHttpRequest()
+    xhttp.onload = function() {
+        myFunction(this)
+    }
+    xhttp.open("GET", "car.xml")
+    xhttp.send()
+}
 
-*/
+function myFunction(xml) {
+    const xmlDoc = xml.responseXML
+    const x = xmlDoc.getElementsByTagName("CAR")
+    let table = "<tr><th>Marque</th></tr>"
+    for (let i = 0; i < x.length; i++) {
+        table += 
+            "<tr><td>" + 
+            x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue + "</td></tr>"
+    }
+    document.getElementById("demo").innerHTML = table
+}
 
-console.log('----------------------------------------')
+console.log("----------------------------------------");
 
 // Exemple 4
 /*
 
 */
 
-console.log('----------------------------------------')
+console.log("----------------------------------------");
 
 // Exemple 5
 /*
 
 */
 
-console.log('----------------------------------------')
+console.log("----------------------------------------");
 
 // Exemple 6
 /*
 
 */
 
-console.log('----------------------------------------')
+console.log("----------------------------------------");
 
 // Exemple 7
 /*
 
 */
 
-console.log('----------------------------------------')
+console.log("----------------------------------------");
--- 01_Studi/04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/05_Ex_Defi/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/03_JSON_AJAX_Format/05_Ex_Defi/01_My_Solution/script.js
@@ -1,1 +1,39 @@
 // Ma solution
+//Notre requête AJAX
+const xhttp = new XMLHttpRequest();
+let city;
+xhttp.onload = function () {
+  const xmlDoc = xhttp.responseXML;
+  city = xmlDoc.getElementsByTagName("CITY");
+  loadCity();
+};
+xhttp.open("GET", "city.xml");
+xhttp.send();
+
+//Notre fonction pour analyser notre XML et afficher sous forme de tableau
+function loadCity() {
+  let table = "<tr><th>Ville</th><th>Code postal</th></tr>";
+  for (let i = 0; i < city.length; i++) {
+    table += "<tr onclick='displayCity(" + i + ")'><td>";
+    table += city[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue;
+    table += "</td><td>";
+    table += city[i].getElementsByTagName("ZIP")[0].childNodes[0].nodeValue;
+    table += "</td></tr>";
+  }
+  document.getElementById("demo").innerHTML = table;
+}
+
+//Notre fonction pour afficher les informations pour chaque ville
+function displayCity(i) {
+  document.getElementById("showCity").innerHTML =
+    "Dépôt : " +
+    city[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
+    "<br>Code postal : " +
+    city[i].getElementsByTagName("ZIP")[0].childNodes[0].nodeValue +
+    "<br>Adresse : " +
+    city[i].getElementsByTagName("ADRESS")[0].childNodes[0].nodeValue +
+    "<br>Jour : " +
+    city[i].getElementsByTagName("DAY")[0].childNodes[0].nodeValue +
+    "<br>Horaire : " +
+    city[i].getElementsByTagName("HOUR")[0].childNodes[0].nodeValue;
+}
