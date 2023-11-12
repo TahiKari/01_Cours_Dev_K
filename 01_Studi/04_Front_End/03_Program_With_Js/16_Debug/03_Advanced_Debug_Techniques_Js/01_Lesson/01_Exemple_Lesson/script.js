4de9efedf36909632acc26d1c46414f11935198e
TAHKAR <tahkar@outlook.fr>
2023-11-12 08:57:14 +0100

    Add lesson 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/02_Intro_Debugging_JS and add lesson 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/03_Advanced_Debug_Techniques_Js and add lesson 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/05_Ex_Defi and add lesson 01_Studi/04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/01_Context and add lesson 01_Studi/04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/04_Esential

--- 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/03_Advanced_Debug_Techniques_Js/01_Lesson/01_Exemple_Lesson/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/03_Advanced_Debug_Techniques_Js/01_Lesson/01_Exemple_Lesson/script.js
@@ -1,34 +1,68 @@
 // Exemple 1
 /*
+function addNumbers(a, b) {
+  return a + b;
+}
 
+var result = addNumbers(2, "3");
+console.log(result);
 */
 
 console.log('----------------------------------------')
 
 // Exemple 2
 /*
+function addNumbers(a, b) {
+   console.log(a);
+  console.log(b);
+  var result = a + b;
+  console.log("result = " + result);
+  return result;
+}
 
+var result = addNumbers(2, "3");
+console.log("resultat final = " + result);
 */
 
 console.log('----------------------------------------')
 
 // Exemple 3
 /*
+function addNumbers(a, b) {
+  console.log(a);
+  console.log(b);
+  var result = Number(a) + Number(b);
+  console.log("result = " + result);
+  return result;
+}
 
+var result = addNumbers(2, "3");
+console.log("resultat final = " + result);
 */
 
 console.log('----------------------------------------')
 
 // Exemple 4
 /*
+function addNumbers(a, b) {
+  return a + b;
+}
 
+var result = addNumbers(2, "3");
+console.log(result);
 */
 
 console.log('----------------------------------------')
 
 // Exemple 5
 /*
+function addNumbers(a, b) {
+  debugger; // Ajout d'un point d'arrêt ici
+  return a + b;
+}
 
+var result = addNumbers(2, "3");
+console.log(result);
 */
 
 console.log('----------------------------------------')
--- 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/05_Ex_Defi/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/16_Debug/05_Ex_Defi/01_My_Solution/script.js
@@ -1,1 +1,27 @@
 // Ma solution
+// Ecouter l'événement de clic sur le bouton de soumission
+document
+  .querySelector('#submit-btn')
+  .addEventListener('click', function (event) {
+    event.preventDefault()
+    // Obtenir les valeurs des champs de saisie
+    var name = document.querySelector('#name').value
+    var email = document.querySelector('#email').value
+    var message = document.querySelector('#message').value
+
+    // Vérifier si les champs sont vides
+    if (name === '' || email === '' || message === '') {
+      alert('Veuillez remplir tous les champs')
+      return
+    }
+
+    // Vérifier si l'adresse e-mail est valide
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(email)) {
+      alert('Adresse e-mail invalide')
+      return
+    }
+
+    // Afficher une alerte pour indiquer que le formulaire a été soumis avec succès
+    alert('Le formulaire a été soumis avec succès !')
+  })
