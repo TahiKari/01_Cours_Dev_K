49447f6b951a18859adb1031ec6e05bcba588fd5
TAHKAR <tahkar@outlook.fr>
2023-11-13 10:06:30 +0100

    add lesson 04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/02_Intro_Js_Best_Practices/02_Airbnb_Style_Guide and add lesson 04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/03_Some_Best_Practice_Js and add lesson 04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/05_Ex_Defi and add lesson 04_Front_End/03_Program_With_Js/18_Project_Memory_Game/01_Context and add lesson 04_Front_End/03_Program_With_Js/18_Project_Memory_Game/04_For_Further

--- 01_Studi/04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/03_Some_Best_Practice_Js/01_Lesson/01_Exemple_Lesson/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/03_Some_Best_Practice_Js/01_Lesson/01_Exemple_Lesson/script.js
@@ -1,48 +1,146 @@
 // Exemple 1
 /*
+// Bonne pratique
+const PI = 3.14159;
+let message = "Bonjour";
 
+// Mauvaise pratique
+var x = 10;
 */
-
-console.log('----------------------------------------')
 
 // Exemple 2
 /*
+// Bonne pratique
+const add = (a, b) => a + b;
 
+// Mauvaise pratique
+function add(a, b) {
+return a + b;
+}
 */
-
-console.log('----------------------------------------')
 
 // Exemple 3
 /*
+// Bonne pratique
+const name = "John";
+const message = `Bonjour, ${name}!`;
 
+// Mauvaise pratique
+const name = "John";
+const message = "Bonjour, " + name + "!";
 */
-
-console.log('----------------------------------------')
 
 // Exemple 4
 /*
+// Bonne pratique
+const fruits = ["pomme", "banane", "orange"];
 
+for (const fruit of fruits) {
+console.log(fruit);
+}
+
+fruits.forEach(fruit => console.log(fruit));
 */
-
-console.log('----------------------------------------')
 
 // Exemple 5
 /*
+// Mauvaise pratique
+const fruits2 = ["pomme", "banane", "orange"];
 
+for (const index in fruits2) {
+console.log(fruits2[index]);
+}
 */
 
-console.log('----------------------------------------')
+/*
+// Bonne pratique
+class Person {
+constructor(name) {
+this.name = name;
+}
+}
+
+const john = new Person("John");
+
+const person = { name: "John" };
+
+// Mauvaise pratique
+function Person(name) {
+this.name = name;
+}
+
+const john = new Person("John");
+
+const person = new Object({ name: "John" });
+*/
 
 // Exemple 6
 /*
+// filter : permet de filtrer les éléments d’un tableau selon un critère
+const numbers1 = [1, 2, 3, 4, 5];
 
+const evenNumbers = numbers1.filter(number => number % 2 === 0)
+console.log(evenNumbers); // [2, 4]
+
+// map : permet de transformer chaque élément d’un tableau en un autre élément
+const numbers2 = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map(number => number * 2);
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+// reduce : permet de réduire les éléments d’un tableau en une seule valeur
+const numbers3 = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((accumulator, number) => accumulator + number, 0);
+console.log(sum); // 15
 */
-
-console.log('----------------------------------------')
 
 // Exemple 7
 /*
+// Bonne pratique
+const matrix = [[1, 2], [3, 4], [5, 6]];
 
+const flattenedMatrix = matrix.flatMap(row => row);
+console.log(flattenedMatrix); // [1, 2, 3, 4, 5, 6]
 */
 
-console.log('----------------------------------------')
+/*
+// Mauvaise pratique
+const matrix = [[1, 2], [3, 4], [5, 6]];
+const flattenedMatrix = [];
+
+for (let i = 0; i < matrix.length; i++) {
+for (let j = 0; j < matrix[i].length; j++) {
+flattenedMatrix.push(matrix[i][j]);
+}
+}
+
+console.log(flattenedMatrix); // [1, 2, 3, 4, 5, 6]
+*/
+
+// Exemple 8
+/*
+// Bonne pratique
+const person = {
+name: "John",
+age: 30,
+address: {
+city: "Paris",
+country: "France"
+}
+};
+
+const country = person.address?.country;
+
+// Mauvaise pratique
+const person = {
+name: "John",
+age: 30,
+address: {
+city: "Paris",
+country: "France"
+}
+};
+
+const country = person.address && person.address.country;
+*/
--- 01_Studi/04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/05_Ex_Defi/01_My_Solution/script.js
+++ 01_Studi/04_Front_End/03_Program_With_Js/17_Best_Practice_Program_JS/05_Ex_Defi/01_My_Solution/script.js
@@ -1,1 +1,38 @@
 // Ma solution
+/*
+const data = [
+  { id: 1, value: 'Item 1' },
+  { id: 2, value: 'Item 2' },
+  { id: 3, value: 'Item 3' }
+]
+
+const resultElement = document.getElementById('result')
+
+function processItems () {
+  data.forEach(item => {
+    const listItem = document.createElement('li')
+    listItem.innerHTML = `ID : ${item.id}, Value : ${item.value}`
+    resultElement.appendChild(listItem)
+
+    item.processed = true
+  })
+}
+
+function checkProcessedItems () {
+  const allProcessed = data.every(item => item.processed)
+  return allProcessed ? 'All items processed' : 'Some items not processed'
+}
+
+processItems()
+console.log(checkProcessedItems())
+*/
+
+const products = [
+  { name: 'T-shirt', price: 20 },
+  { name: 'Jeans', price: 50 },
+  { name: 'Sneakers', price: 80 }
+];
+
+for (const product of products) {
+  console.log(`${product.name} - ${product.price}`);
+}
