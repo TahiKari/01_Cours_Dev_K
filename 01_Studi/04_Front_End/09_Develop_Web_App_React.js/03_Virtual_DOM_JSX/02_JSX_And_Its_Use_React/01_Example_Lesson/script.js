19a6a982b7d1fac7dc60f0061293ef56cb025486
TAHKAR <tahkar@outlook.fr>
2023-11-18 06:40:34 +0100

    All lesson 09_Develop_Web_App_React.js/03_Virtual_DOM_JSX/02_JSX_And_Its_Use_React and add lesson 09_Develop_Web_App_React.js/03_Virtual_DOM_JSX/03_Virtual_DOM_React

---
+++ 01_Studi/04_Front_End/09_Develop_Web_App_React.js/03_Virtual_DOM_JSX/02_JSX_And_Its_Use_React/01_Example_Lesson/script.js
@@ -0,0 +1,92 @@
+// Exemple 2
+/*
+<a href={url}>Cliquez ici</a>
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 3
+/*
+<h1 style={{color: isRed ? 'red' : 'blue'}}>{title}</h1>
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 4
+/*
+const element = <h1>Hello, world!</h1>
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 5
+/*
+const element = <img src="image.jpg" alt="description" />
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 6
+/*
+const element = <p>Hello, world!</p>
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 7
+/*
+const element = (
+  <div>
+    <h1>My Title</h1>
+    <p>Some text here.</p>
+  </div>
+)
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 8
+/*
+const name = "Arthur";
+const element = <p>Hello, {name}!</p>;
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 9
+/*
+const element = <h1 className="monTitre">Hello, world!</h1>;
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 10
+/*
+const element = React.createElement(
+  'h1',
+  {className: 'monTitre'},
+  'Hello, world!'
+)
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 11
+/*
+<button className="btn" onClick={maSuperFonctionAppuyee}>
+  Cliquez-moi
+</button>
+à la place de :
+<button className="btn">
+  Cliquez-moi
+</button>
+*/
+
+console.log('-------------------------------------')
+
+// Exemple 12
+/*
+{isLogged ? <button>Se déconnecter</button> : <button>Se connecter</button>}
+*/
+
+console.log('-------------------------------------')
---
+++ 01_Studi/04_Front_End/09_Develop_Web_App_React.js/03_Virtual_DOM_JSX/02_JSX_And_Its_Use_React/02_JSX/src/App.js
@@ -0,0 +1,4 @@
+export default function App() {
+  const element = <h1>Hello JSX</h1>
+  return element
+}
