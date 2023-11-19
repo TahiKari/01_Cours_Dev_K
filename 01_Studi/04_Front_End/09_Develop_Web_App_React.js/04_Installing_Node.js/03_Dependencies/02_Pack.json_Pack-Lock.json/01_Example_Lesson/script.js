a741693d36582d4b1f802382f02c841c468c4d56
TAHKAR <tahkar@outlook.fr>
2023-11-19 10:27:26 +0100

    Add video lesson 01_Studi/04_Front_End/09_Develop_Web_App_React.js 01 a 04

---
+++ 01_Studi/04_Front_End/09_Develop_Web_App_React.js/04_Installing_Node.js/03_Dependencies/02_Pack.json_Pack-Lock.json/01_Example_Lesson/script.js
@@ -0,0 +1,21 @@
+// Ma solution
+/*
+{
+	"name" : "mon-projet",
+	"version" : "1.0.0",
+	"description" : "",
+	"main" : "index.js",
+	"scripts":{
+		"dev": "react-scripts start",
+		"build": "react-scripts build"
+},
+"keywords" : [],
+"author" : "",
+"license" : "ISC",
+"dependencies": {
+	"express" : "^4.18.2"
+}
+}
+*/
+
+console.log('-------------------------------------')
