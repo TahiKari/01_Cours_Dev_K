cb19c14e41506b23eb038e46e5ae6cb808f63051
TAHKAR <tahkar@outlook.fr>
2024-01-01 11:19:14 +0100

    Add lesson 01_Studi/04_Front_End/09_Develop_Web_App_React.js/22_Project_Develop_Web_App_React.js

---
+++ 01_Studi/04_Front_End/09_Develop_Web_App_React.js/22_Project_Develop_Web_App_React.js/05_Project/01_app_meteo/src/API_KEY.js
@@ -0,0 +1,3 @@
+export const weatherKey = '147f403dc47f8b0e3f3740120048298b'
+
+export const timeDbKey = 'VRMQNO7972TT'
---
+++ 01_Studi/04_Front_End/09_Develop_Web_App_React.js/22_Project_Develop_Web_App_React.js/05_Project/01_app_meteo/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header.js'
+import Meteo from './components/Meteo/Meteo.js'
+import CityInpunt from './components/CityInput/CityInput.js'
+
+function App () {
+  return (
+    <>
+      <Header />
+      <Meteo />
+      <CityInpunt />
+    </>
+  )
+}
+
+export default App
